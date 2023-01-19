7033b52d072a806d6037340e714990bbe3f8d5d5
UntitledProgrammer <thomasjacobs0257@gmail.com>
2023-01-19 09:28:27 +0000

    Update.

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FelineFrenzy.Inventory
+{
+    public class Inventory : ScriptableObject
+    {
+        //Attributes:
+        private uint balance;
+
+        //Properties:
+        public uint Balance { get => balance; }
+
+        //Methods:
+        public bool Spend(uint amount)
+        {
+            if (amount > balance) return false;
+            balance -= amount;
+            return true;
+        }
+
+        public void Add(uint amount) => balance += amount;
+    }
+}
---
+++ Assets/Scripts/Inventory/Magnet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    //Attributes:
+    public float radius;
+    public float attraction;
+    public LayerMask mask;
+    private List<GameObject> targets;
+    private const float tolerance = 0.2f;
+
+    //Methods:
+    private void Start() => targets = new List<GameObject>();
+
+    private void Update()
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, radius, Vector2.one, 0.0f);
+        if (hit.collider != null)
+        {
+            hit.collider.enabled = false;
+            targets.Add(hit.collider.gameObject);
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (targets.Count <= 0) return;
+
+        for (int i = targets.Count - 1; i >= 0; i--)
+        {
+            Vector2 direction = (transform.position - targets[i].transform.position).normalized;
+            targets[i].transform.position += Time.deltaTime * attraction * (Vector3)direction;
+
+            if(Vector2.Distance(targets[i].transform.position, transform.position) <= tolerance)
+            {
+                GameObject temp = targets[i];
+                targets.RemoveAt(i);
+                Destroy(temp);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
