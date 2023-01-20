443e2e770c5be7b11c4651e5d1d394a4ffe84d0c
UntitledProgrammer <thomasjacobs0257@gmail.com>
2023-01-20 19:09:43 +0000

    Chunk Optimisation.

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -2,26 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chunk : MonoBehaviour
+namespace FelineFrenzy.Core
 {
-    //Attributes:
-    public Vector2 flag_position;
-    public Vector2 perimeter;
-    public Vector2 offset;
-    private const float radius = 0.25f;
-
-    //Properties:
-    public Vector2 FlagPosition { get => (Vector2)transform.position + flag_position; }
-    public Vector2 Centre { get => perimeter + offset; }
-
-    //Methods:
-    public virtual void Spawn() {  }
-    public virtual void Despawn() {  }
-
-    public void OnDrawGizmos()
+    /**
+    *  A chunk is a collection of gameObjects that collectively produce a segment of an overall level.
+    * 
+    *  @project Feline Frenzy.
+    *  @owner Thomas Jacobs | S212046.
+    */
+    public class Chunk : MonoBehaviour
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere((Vector2)transform.position + flag_position, radius);
-        Gizmos.DrawWireCube(transform.position + (Vector3)offset, (Vector3)perimeter);
+        //Attributes:
+        public float flag;
+        public float extent;
+        public Vector2 pivot;
+        private const float flag_length = 1.0f;
+
+        //Properties:
+        public Vector2 Left { get => ((Vector2)transform.position + pivot) + Vector2.left * extent; }
+        public Vector2 FlagPosition { get => Left + Vector2.right * flag; }
+        public float Length { get => extent + extent; }
+        public Vector2 Right { get => ((Vector2)transform.position + pivot) + Vector2.right * extent; }
+        public Vector2 Offset { get => (Vector2)transform.position + pivot; }
+        public Vector2 Centre { get => ((Vector2)transform.position + pivot); }
+
+        //Methods:
+        public virtual void Spawn() { }
+        public virtual void Despawn() { }
+
+        public void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(Left, Right);
+            Gizmos.DrawLine(FlagPosition, FlagPosition + Vector2.up * flag_length);
+        }
     }
 }
--- Assets/Scripts/ChunkLoader.cs
+++ Assets/Scripts/ChunkLoader.cs
@@ -2,37 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class ChunkLoader : MonoBehaviour
+namespace FelineFrenzy.Core
 {
-    //Attributes:
-    public Chunk start;
-    public Transform player_transform;
-    public List<Chunk> chunks = new List<Chunk>();
-    private const float tolerance = 1.0f;
-
-    //Methods:
-    private float Distance(Vector2 a, Vector2 b)
+    /**
+    *   The chunk loader handles the continual, random progression of a level.
+    * 
+    *   @owner Thomas Jacobs | S212046.
+    *   @project Feline Frenzy.
+    */
+    public class ChunkLoader : MonoBehaviour
     {
-        a.y = 0.0f;
-        b.y = 0.0f;
+        //Attributes:
+        public Chunk start;
+        public Transform player_transform;
+        public List<Chunk> chunks = new List<Chunk>();
+        private const float tolerance = 1.0f;
+        private const float delay = 8.0f;
 
-        return Vector2.Distance(a, b);
-    }
-    
-    private Chunk NextChunk()
-    {
-        return chunks[Random.Range(0, chunks.Count)];
-    }
+        //Methods:
+        private float Distance(Vector2 a, Vector2 b)
+        {
+            a.y = 0.0f;
+            b.y = 0.0f;
 
-    private void Update()
-    {
-        if (Distance(start.FlagPosition, player_transform.position) > tolerance) return;
+            return Vector2.Distance(a, b);
+        }
 
-        //Player has passed flag...
-        Chunk temp = GameObject.Instantiate(NextChunk().gameObject).GetComponent<Chunk>();
-        temp.transform.position = start.transform.position + Vector3.right * start.perimeter.x;
-        temp.transform.SetParent(transform);
-        start = temp;
+        private Chunk NextChunk()
+        {
+            return chunks[Random.Range(0, chunks.Count)];
+        }
+
+        private void Update()
+        {
+            if (Distance(start.FlagPosition, player_transform.position) > tolerance) return;
+
+            //Player has passed flag...
+            Chunk temp = GameObject.Instantiate(NextChunk().gameObject).GetComponent<Chunk>();
+            temp.transform.position = start.Right + Vector2.right * temp.extent;
+            temp.transform.SetParent(transform);
+
+            Destroy(start.gameObject, delay);
+
+            start = temp;
+        }
     }
 }
