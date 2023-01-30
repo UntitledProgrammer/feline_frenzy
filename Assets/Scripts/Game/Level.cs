77e89211d0e84376bf34ad0fc9de7448b4b6d64e
untitledprogrammer <thomasjacobs0257@gmail.com>
2023-01-30 13:28:45 +0000

    Update.

---
+++ Assets/Scripts/Game/Level.cs
@@ -0,0 +1,15 @@
+﻿public struct Level
+{
+    //Attributes:
+    public string sceneName;
+    public float recordTime;
+    public bool unlocked;
+
+    //Constructor:
+    public Level(string sceneName, bool unlocked = false, float recordTime = 0.0f)
+    {
+        this.sceneName = sceneName;
+        this.recordTime = recordTime;
+        this.unlocked = unlocked;
+    }
+}
---
+++ Assets/Scripts/Game/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LevelManager
+{
+    //Attributes:
+    private List<Level> levels;
+    private const string filename = "test.json";
+    private static LevelManager singleton;
+
+    //Constructor:
+    public LevelManager() { levels = new List<Level>(); }
+
+    //Properties:
+    public LevelManager Singleton
+    {
+        get
+        {
+            return singleton;
+        }
+    }
+
+    //Methods:
+    public bool Load()
+    {
+        if (!File.Exists(filename)) return false;
+
+        LevelManager data = JsonUtility.FromJson<LevelManager>(File.ReadAllText(filename));
+        levels = data.levels;
+
+        return true;
+    }
+
+    public void Save()
+    {
+        File.WriteAllText(filename, JsonUtility.ToJson(this));
+    }
+}
