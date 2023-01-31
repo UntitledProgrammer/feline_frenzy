c59ccdb8670a225223b628a1ae600e242b879fcb
UntitledProgrammer <thomasjacobs0257@gmail.com>
2023-01-31 11:22:25 +0000

    Update.

---
+++ Assets/Scenes/SceneButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class SceneButton : Button
+{
+    //Attributes:
+    public SceneInfo sceneInfo;
+
+    //Methods:
+    private void LoadScene()
+    {
+        Debug.Log("Click");
+
+        if (sceneInfo == null || !sceneInfo.unlocked) return;
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneInfo.Name);
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        onClick.AddListener(LoadScene);
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(SceneButton))]
+public class SceneButtonEditor : Editor
+{
+    //Attributes:
+    private SceneButton self;
+
+    //Methods:
+    private void OnEnable() => self = (SceneButton)target;
+
+    public override void OnInspectorGUI()
+    {
+        self.sceneInfo = (SceneInfo)EditorGUILayout.ObjectField(self.sceneInfo, typeof(SceneInfo));
+        DrawDefaultInspector();
+    }
+}
+#endif
---
+++ Assets/Scenes/SceneInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu(fileName = "SceneInfo", menuName = "SceneInfo", order = 0)]
+public class SceneInfo : ScriptableObject
+{
+    //Attributes:
+    public string sceneName;
+    public bool unlocked;
+    public float record;
+
+    //Properties:
+    public string Name { get => sceneName; }
+
+    //Methods:
+    public void Awake()
+    {
+        unlocked = false;
+        record = 0.0f;
+        sceneName = Selection.activeObject is SceneAsset ? ((SceneAsset)Selection.activeObject).name : "empty";
+    }
+}
