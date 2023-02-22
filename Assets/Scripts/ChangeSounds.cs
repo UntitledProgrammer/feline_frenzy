d9320f4861db60a6d1cce0b5a4a7177cfa472a40
UntitledProgrammer <tiureanstefana@gmail.com>
2023-02-22 20:02:22 +0000

    22/02 update
    level selector and main menu update

---
+++ Assets/Scripts/ChangeSounds.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeSounds : MonoBehaviour
+{
+    private Sprite SoundOnImage;
+    public Sprite SoundOffImage;
+    public Button button;
+    private bool isOn = true;
+
+    public AudioSource backgroundMusicSource;
+    public AudioSource buttonSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SoundOnImage = button.image.sprite;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+public void ButtonClicked() 
+    {
+        if (isOn)
+        {
+            button.image.sprite = SoundOffImage;
+            isOn = false;
+            backgroundMusicSource.mute = true;
+            buttonSource.mute = true;
+        }
+        else
+        {
+            button.image.sprite = SoundOnImage;
+            isOn = true;
+            backgroundMusicSource.mute = false;
+            buttonSource.mute = false;
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    public GameObject playButton;
+
+    public GameObject storyModeButton;
+    public GameObject survivalModeButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ButtonChange()
+    {
+        storyModeButton.SetActive(true);
+        survivalModeButton.SetActive(true);
+        playButton.SetActive(false);
+    }
+
+    public void StoryModeButton()
+    {
+        SceneManager.LoadScene("LevelSelect");
+    }
+}
